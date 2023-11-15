a4169a75c552b99c4d7eff4bf4ee9f0d94e623f0
Feda Muhammadian <fedamuhammadian@Fedas-MacBook-Air.local>
2023-11-15 12:19:25 -0800

    mode some design changes for better UI experience

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -8,33 +8,32 @@ import PhotoList from 'components/PhotoList';
 import PhotoFavButton from 'components/PhotoFavButton';
 
 const PhotoDetailsModal = ({ photo, onClose, toggleFavourite }) => {
-
   const similarPhotos = Object.values(photo.similar_photos);
+
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={onClose}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
-      <div>
-        <PhotoFavButton
-          photo={photo}
-          toggleFavourite={toggleFavourite} />
-      </div>
-      <div className="photo-details-modal__image" />
-      <img src={photo.urls.full} className="photo-details-modal__image" alt="SelectedPhoto" />
-      <div className="photo-details-modal__top-bar" />
-      <div className="photo-details-modal__photographer-details">
-        <img src={photo.user.profile} className="photo-list__user-profile" alt="ProfilePhoto" />
-        <div className="photo-list__user-info">
-          <p className="photo-list__user-info">{photo.user.name}</p>
-          <p className="photo-list__user-info photo-list__user-location">{photo.location.city}, {photo.location.country}</p>
+      <div className="photo-details-modal__images">
+        <div>
+          <PhotoFavButton
+            toggleFavourite={toggleFavourite} />
         </div>
+        {/* <div className="photo-details-modal__image" /> */}
+        <img src={photo.urls.full} className="photo-details-modal__image" alt="SelectedPhoto" />
+        <div className="photo-details-modal__top-bar" />
+        <div className="photo-details-modal__photographer-details">
+          <img src={photo.user.profile} className="photo-list__user-profile" />
+          <section className="photo-list__user-info">
+            <div className="photo-list__user-info">{photo.user.name}</div>
+            <div className="photo-list__user-info photo-list__user-location">{photo.location.city}, {photo.location.country}</div>
+          </section>
+        </div>
+        <div className="photo-details-modal__header">Similar Photos</div>
+        <PhotoList photos={similarPhotos} toggleFavourite={toggleFavourite} />
       </div>
-      <h2 className="similar-photos"></h2>
-      <h3 className="photo-details-modal__header">Similar Photos</h3>
-      <PhotoList photos={similarPhotos} />
     </div>
-
   )
 };
 
