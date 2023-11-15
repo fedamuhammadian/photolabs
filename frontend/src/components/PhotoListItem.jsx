fa6cb82e3aa8e6325326cfba3374bc82a5b183cf
Feda Muhammadian <fedamuhammadian@Fedas-MacBook-Air.local>
2023-11-14 22:06:11 -0800

    applied styling to components and refactored them

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -5,17 +5,23 @@ import "../styles/PhotoListItem.scss";
 
 
 
-const PhotoListItem = () => {
+const PhotoListItem = (props) => {
   const { id, location, user } = props.data;
   const { regular, profile } = props.data.urls;
 
   return (
     <div className="photo-list-item">
-      <img src={regular} alt={`Photo by ${user.username}`} />
+      <img src={regular} alt={`Photo by ${user.username}`} className="photo-list__image" />
       <h3>{`Photo ${id}`}</h3>
-      <p>{`Location: ${location.city}, ${location.country}`}</p>
-      <p>{`Uploaded by: ${user.username}`}</p>
-      <img src={profile} alt={`${user.username}'s profile`} />
+      <div className="photo-list__user-details ">
+        <img src={user.profile} className="photo-list__user-profile" />
+        <div>
+          <p className="photo-list__user-info">{user.name}</p>
+          <p className="photo-list__user-location">
+            {location.city}, {location.country}
+          </p>
+        </div>
+        </div>
     </div>
   );
 };
